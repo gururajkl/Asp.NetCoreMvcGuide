97e99bb23785489bb6de64201e83f12caaf639b4
GururajKL <gururaj.kl@sasken.com>
2023-11-08 18:09:31 +0530

    Added Update logic.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -19,8 +19,52 @@ namespace BulkyWeb.Controllers
             return View(categoriesList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            /* For Custom Validation (Use ModelState.AddModelError()). */
+
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category? categoryFromDb = db.Categories.Find(Id);
+                if (categoryFromDb == null)
+                {
+                    return NotFound();
+                }
+                return View(categoryFromDb);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
             return View();
         }
     }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,9 +8,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30, ErrorMessage = "Category name is too big")]
         [DisplayName("Category Name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The Display order should be in between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
