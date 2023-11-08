047b02c6803155847f2c230068d2c66492fd6999
Gururaj KL <gururaj.kl@sasken.com>
2023-11-08 22:03:06 +0530

    Started to work on Razor Pages.

---
+++ BulkyWebRazor/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Category name is too big")]
+        [DisplayName("Category Name")]
+        public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The Display order should be in between 1 and 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
